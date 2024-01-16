88336d1d02f1e7e3c285e8da747290fcde61bec5
tanmaymunjal <tanmaymunjal64@gmail.com>
2024-01-16 08:46:40 +0000

    Update to Mp3

---
+++ Scrapper/dataset_to_parse.py
@@ -0,0 +1,21 @@
+youtube_channel_ids = {
+    "PRSundar64":"UCS2NdYUmv_PUyyKeDAo5zYA",
+    "pranjalkamra":"UCwAdQUuPT6laN-AQR17fe1g",
+    "LabourLawAdvisor":"UCVOTBwF0vnSxMRIbfSE_K_g",
+    "rachanaphadke": "UCe3qdG0A_gr-sEdat5y2twQ",
+    "AssetYogi": "UCsNxHPbaCWL1tKw2hxGQD6g",
+    "ManojkumarJain": "UCHXTbd8Av-HR3Yng65dfCZg",
+    "SunilMinglani": "UCS8WLwuVszq1g2oeBzboUmQ",
+    "VivekBajaj": "UCBnG19e3YPT3OTMY7uFfu7A",
+    "MukulAgrawal": "UCPs8w9f1gqe4BqkbI-9wTnw",
+    "wealthsagaglobal": "UC1Jo6UXxw-hh8atJdLYQthw",
+    "cnbcawaaz": "UCQIycDaLsBpMKjOCeaKUYVg",
+    "zeebusiness": "UCkXopQ3ubd-rnXnStZqCl2w",
+    "NDTVProfitIndia": "UC3uJIdRFTGgLWrUziaHbzrg",
+    "NitinBhatia": "UCthN3CTgZY0WIE9A5u1Qcew",
+    "BWealthy": "UCthN3CTgZY0WIE9A5u1Qcew",
+    "Groww": "UCw5TLrz3qADabwezTEcOmgQ",
+    "MOFSL": "UCk8EZaxKvAi1a5AEe4Hobmg",
+    "ICICIPrudentialAMC": "UClGyZZrTsCNqUl99morKSXQ",
+    "Nuvama_Wealth": "UC0ik1TOToNoGQdPsZlDfVPA"
+}
--- Scrapper/youtube_scrapper.py
+++ Scrapper/youtube_scrapper.py
@@ -3,9 +3,12 @@ import os
 from googleapiclient.discovery import build
 from pytube import YouTube
 import configparser
+import pathlib
+from dataset_to_parse import youtube_channel_ids
 
+config_path = pathlib.Path(__file__).parent.absolute() / "config.ini"
 config = configparser.ConfigParser()
-config = config.read("../config.ini")
+config.read(config_path)
 API_KEY = config["YoutubeAPI"]["API_KEY"]
 
 
@@ -20,7 +23,7 @@ class YoutubeScrapper(Scrapper):
     def scrap(self, max_results: int = 50):
         video_ids = YoutubeScrapper.get_youtube_video_list(self.user_id, max_results)
         for video_id in video_ids:
-            YoutubeScrapper.get_audio(video_id)
+            YoutubeScrapper.get_audio(video_id,output_path=f"../audios/{self.user_id}")
 
     @staticmethod
     def get_youtube_video_list(channel_id, max_count=50):
@@ -39,6 +42,7 @@ class YoutubeScrapper(Scrapper):
                     type="video",
                     maxResults=50,
                     pageToken=next_page_token,
+                    order = "viewCount"
                 )
                 .execute()
             )
@@ -67,14 +71,20 @@ class YoutubeScrapper(Scrapper):
 
     @staticmethod
     def get_audio(
-        video_id: str, audio_extension: str = "mp4", output_path="../audios"
+        video_id: str, audio_extension: str = "mp3", output_path="../audios"
     ) -> bool:
+        print("wOW")
         youtube_url = f'https://www.youtube.com/watch?v={video_id}"'
         try:
             video = YouTube(youtube_url)
-            audio = video.streams.filter(only_audio=True, file_extension="mp4").first()
-            audio.download(output_path=f"../{output_path}/{video_id}")
+            audio = video.streams.filter(only_audio=True, file_extension="mp3").first()
+            audio.download(output_path=output_path)
             return True
         except Exception as err:
             print(repr(err))
             return False
+
+
+for value in youtube_channel_ids.values():
+    youtube = YoutubeScrapper(value)
+    youtube.scrap(100)
